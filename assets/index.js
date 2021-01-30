995a37ef648a2fe2fd09afc867284cae2a818f02
Noah Schlag <noahschlag12@gmail.com>
2021-01-30 16:52:41 -0600

    added questions and variables into the js documents

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+// var index of the questions array
+var questionIndex = 0;
+var timerId;
+// these variables are going to represent DOM elements
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+
---
+++ assets/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+       title: "Commonly used data types do not include:", 
+       choices: ["strings", "booleans", "alerts", "numbers"],
+       answer: "alerts"  
+    },
+    {
+        title: "arrays in JS can be used to store:",
+        choices: ["numbers & strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "condition in an if/else statement is enclosed within _______",
+        choices: ["quote", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+]
